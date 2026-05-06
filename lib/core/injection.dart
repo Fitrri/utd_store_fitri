9b61ae02d12b652b2b8a0799c43394ca85e794f0
Fitrri <fffitrianisa2005@gmail.com>
2026-05-06 14:46:50 +0700

    chore: final finishing, splash screen implementation, and dio interceptor logging
    Menyelesaikan seluruh requirement ETS: Splash screen (delay 5s), Dio Interceptor logging, dan pembersihan kode akhir.

--- lib/core/injection.dart
+++ lib/core/injection.dart
@@ -1,8 +1,8 @@
 import 'package:get_it/get_it.dart';
 import 'package:dio/dio.dart';
-import 'package:isar/isar.dart'; // Import Isar
-import 'package:path_provider/path_provider.dart'; // Import Path Provider
-import '../domain/product_model.dart'; // Import Model untuk Schema
+import 'package:isar/isar.dart'; 
+import 'package:path_provider/path_provider.dart'; 
+import '../domain/product_model.dart'; 
 import '../domain/splash_service.dart';
 import '../data/product_repository.dart';
 import '../presentation/cubit/product_cubit.dart';
@@ -11,19 +11,45 @@ final sl = GetIt.instance;
 
 Future<void> inisialisasi() async {
   // 1. Inisialisasi ISAR (Database Lokal)
-  // Mencari folder dokumen di HP agar database bisa disimpan permanen
   final dir = await getApplicationDocumentsDirectory();
   
   final isar = await Isar.open(
-    [ProductSchema], // Schema dihasilkan otomatis dari build_runner
+    [ProductSchema], 
     directory: dir.path,
   );
 
-  // Daftarkan Isar sebagai Singleton (biar satu database dipakai rame-rame)
   sl.registerSingleton<Isar>(isar);
 
-  // 2. Register Dio (Networking)
-  sl.registerLazySingleton(() => Dio());
+  // 2. Register Dio (Networking) dengan INTERCEPTOR (Poin 2 PDF ETS)
+  sl.registerLazySingleton(() {
+    final dio = Dio();
+    
+    // Ini adalah bagian Interceptor yang WAJIB kamu jelaskan di video
+    dio.interceptors.add(InterceptorsWrapper(
+      onRequest: (options, handler) {
+        // Fungsi: Mencatat log setiap kali aplikasi meminta data ke internet
+        print("--- NETWORK LOG (REQUEST) ---");
+        print("URL: ${options.uri}");
+        print("Method: ${options.method}");
+        return handler.next(options); 
+      },
+      onResponse: (response, handler) {
+        // Fungsi: Mencatat log ketika data berhasil diterima
+        print("--- NETWORK LOG (RESPONSE) ---");
+        print("Status: ${response.statusCode}");
+        print("Data: Berhasil diterima");
+        return handler.next(response);
+      },
+      onError: (DioException e, handler) {
+        // Fungsi: Mencatat log jika terjadi error (misal internet mati)
+        print("--- NETWORK LOG (ERROR) ---");
+        print("Pesan Error: ${e.message}");
+        return handler.next(e);
+      },
+    ));
+    
+    return dio;
+  });
   
   // 3. Register Service
   sl.registerLazySingleton(() => SplashService());
